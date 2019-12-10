a596945d09a137e2a532652d21c453bb20d3af77
Gellipapa <gellerthorvath0@gmail.com>
2019-12-10 17:12:10 +0100

    Megrendelo teszt adatok feltöltés metodusa kész

---
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/repository/RepositoryMegrendeloTableMegrendeloTestData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TobbformosPizzaAlkalmazasEgyTabla.Model;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace TobbformosPizzaAlkalmazasEgyTabla.Repository
+{
+    partial class RepositoryDatabaseTableMegrendelo
+    {
+  
+        /// <summary>
+        /// Feltölti teszt adatokkal az adatbázis megrendelo tábláját
+        /// </summary>
+        public void fillOrdersWithTestDataFromSQLCommand()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+
+                string query =
+                    "INSERT INTO `pmegrendelo` (`id`, `name`,`address`,`par`) VALUES " +
+                            " (1, 'Jancsi','Budapest',1500), " +
+                            " (2, 'Maris','Kalocsa',1100), " +
+                            " (3, 'Károly','Szeged',1800), " +
+                            " (4, 'Béla','Baja' ,1450), " +
+                            " (5, 'Anna','Debrecen' ,990); ";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                Debug.WriteLine(e.Message);
+                throw new RepositoryException("Tesztadatok feltöltése sikertelen.");
+            }
+        }
+    }
+}
