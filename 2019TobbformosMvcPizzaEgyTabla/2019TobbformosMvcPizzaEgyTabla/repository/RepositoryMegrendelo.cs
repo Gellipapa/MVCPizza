72cdec7f656ceaf2e6fc0a2a26cac8b70666616d
Horváth Gellért <gellerthorvath0@gmail.com>
2019-12-10 11:05:19 +0100

    Repositroy megrendelo lista deklarálása set és get

---
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/repository/RepositoryMegrendelo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using TobbbformosPizzaAlkalmazasEgyTabla.Model;
+
+namespace TobbbformosPizzaAlkalmazasEgyTabla.Repository
+{
+    partial class Repository
+    {
+        //megrendelo listája
+        List<Pizza> orders;
+
+        //megrendelo get lista
+        public List<Pizza> getOrders()
+        {
+            return orders;
+        }
+        //megrendelo set lista
+        public void setOrders(List<Pizza>orders)
+        {
+            this.orders = orders;
+        }
+
+    }
+}
